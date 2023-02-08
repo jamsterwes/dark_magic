8585574153fdfb707b944f1021ad1ebc3d197641
Wesley Taylor <jamsterwes@gmail.com>
2023-02-08 00:01:44 -0600

    dimension loading still broken, see "won't load chunk right"

--- src/main/java/wesley/magic/tomes/special/EndlessVoidTomeItem.java
+++ src/main/java/wesley/magic/tomes/special/EndlessVoidTomeItem.java
@@ -5,12 +5,18 @@ import net.minecraft.client.render.DimensionEffects.Overworld;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ChunkTicketType;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionTypes;
+import net.minecraft.world.poi.PointOfInterestStorage;
 import wesley.magic.tomes.TomeProperties;
 import wesley.magic.tomes.UseTomeItem;
+import wesley.magic.ExampleMod;
 
 public class EndlessVoidTomeItem extends UseTomeItem {
 
@@ -27,20 +33,40 @@ public class EndlessVoidTomeItem extends UseTomeItem {
         // Check what dimension we are in
         if (player.getWorld().getRegistryKey() == World.OVERWORLD) {
             // Teleport into endless void
-            player.moveToWorld(player.getServer().getWorld(endlessVoidDim));
+            teleportToDimension(player, endlessVoidDim);
         } else if (player.getWorld().getRegistryKey() == endlessVoidDim) {
             // Teleport back into overworld
-            player.moveToWorld(player.getServer().getOverworld());
+            teleportToDimension(player, World.OVERWORLD);
         }
     }
 
     private void teleportToDimension(ServerPlayerEntity player, RegistryKey<World> dimension) {
         // Get original position
         Vec3d origPos = player.getPos();
-        // TODO: fix scale?
-        Vec3d newPos = origPos.multiply(1.0, 1.0, 1.0);
 
-        player.moveToWorld(player.getServer().getWorld(dimension));
+        // File a chunk ticket
+
+        ServerWorld currentWorld = player.getWorld();
+        ServerWorld targetWorld = player.getServer().getWorld(dimension);
+        // TODO: fix scale?
+
+        Vec3d newPos = origPos.multiply(currentWorld.getDimension().coordinateScale() / targetWorld.getDimension().coordinateScale());
+        BlockPos newPosBlock = new BlockPos((int)newPos.x, (int)newPos.y, (int)newPos.z);
+        ChunkPos newPosChunk = new ChunkPos(newPosBlock);
+        
+        // PointOfInterestStorage pointOfInterestStorage = targetWorld.getPointOfInterestStorage();
+        // pointOfInterestStorage.preloadChunks(targetWorld, newPosBlock, 16 * 3);
+        
+        ExampleMod.LOGGER.info("Loading chunk @ " + newPosChunk.toString());
+
+        targetWorld.getChunkManager().addTicket(
+            ChunkTicketType.PORTAL,
+            newPosChunk,
+            3,
+            newPosBlock
+        );
+
+        player = (ServerPlayerEntity)player.moveToWorld(targetWorld);
         player.setYaw(0.0f);
         player.setPitch(0.0f);
         player.refreshPositionAfterTeleport(newPos);
